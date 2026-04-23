08e1af6ee896f2523f580b05fe0f829be42078d1
Keyue Hu <hukeyue@vip.163.com>
2026-04-23 15:23:53 +0800

    update build-skia.sh

--- build-skia.sh
+++ build-skia.sh
@@ -7,7 +7,7 @@ MACHINE=$(uname -m)
 ARGS="skia_use_icu=true is_debug=false is_official_build=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_expat=false skia_use_system_zlib=false"
 
 ARGS="cc=\"clang\" cxx=\"clang++\" ar=\"llvm-ar\" $ARGS"
-ARGS="clang_win=\"$HOME/clang+llvm-22.1.4-arm64-apple-darwin20.1.0\" clang_win_version=\"20\" $ARGS"
+ARGS="clang_win=\"$HOME/clang+llvm-22.1.4-arm64-apple-darwin20.1.0\" clang_win_version=\"22\" $ARGS"
 ARGS="target_cpu=\"x64\" target_os=\"win\" $ARGS"
 ARGS="current_cpu=\"x64\" current_os=\"win\" $ARGS"
 ARGS="win_vc=\"$HOME/vstoolchain/VC\" win_toolchain_version=\"14.50.35717\" $ARGS"
