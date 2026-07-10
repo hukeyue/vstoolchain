38b5066c79ecf48e5752cf6ccd1df09cded62f15
Keyue Hu <hukeyue@vip.163.com>
2026-07-10 16:18:16 +0800

    skia: but with angle support

--- build-skia.sh
+++ build-skia.sh
@@ -4,7 +4,7 @@ set -e
 
 MACHINE=$(uname -m)
 
-ARGS="skia_use_icu=true is_debug=false is_official_build=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_expat=false skia_use_system_zlib=false"
+ARGS="skia_use_icu=true is_debug=false is_official_build=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_expat=false skia_use_system_zlib=false skia_use_angle=true"
 
 ARGS="cc=\"clang\" cxx=\"clang++\" ar=\"llvm-ar\" $ARGS"
 ARGS="clang_win=\"$HOME/clang+llvm-22.1.8-arm64-apple-darwin20.1.0\" clang_win_version=\"22\" $ARGS"
@@ -13,6 +13,6 @@ ARGS="current_cpu=\"x64\" current_os=\"win\" $ARGS"
 ARGS="win_vc=\"$HOME/vstoolchain/VC\" win_toolchain_version=\"14.50.35717\" $ARGS"
 ARGS="win_sdk=\"$HOME/vstoolchain/Windows_Kits/10\" win_sdk_version=\"10.0.26100.0\" $ARGS"
 
-cd ~/skia
+cd ~/skia-next
 #rm -rf out/win
-gn gen out/win "--args=$ARGS"
+./bin/gn gen out/win "--args=$ARGS"
