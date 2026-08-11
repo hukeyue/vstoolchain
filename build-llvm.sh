d2dd22a717373b32c87632f1536c89189b16ce2c
Keyue Hu <hukeyue@vip.163.com>
2026-08-11 20:48:26 +0800

    llvm: update default triple

--- build-llvm.sh
+++ build-llvm.sh
@@ -7,12 +7,15 @@ WITH_CPU=${WITH_CPU:-x86_64}
 case "$WITH_CPU" in
   x86|i586|i686)
     WITH_CPU="x86"
+    TARGET_TRIPLE="i686-pc-windows-msvc"
     ;;
   x86_64)
     WITH_CPU="x64"
+    TARGET_TRIPLE="x86_64-pc-windows-msvc"
     ;;
   arch64|arm64)
     WITH_CPU="arm64"
+    TARGET_TRIPLE="aarch64-pc-windows-msvc"
     ;;
 esac
 
@@ -24,7 +27,7 @@ cd "$BUILD_DIR"
 LLVM_PATH=$HOME/clang+llvm-22.1.8-arm64-apple-darwin20.1.0
 cmake -G Ninja \
    -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=on \
-   -DLLVM_DEFAULT_TARGET_TRIPLE="$WITH_CPU-windows-msvc" \
+   -DLLVM_DEFAULT_TARGET_TRIPLE="$TARGET_TRIPLE" \
    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
    -DLLVM_BUILD_EXAMPLES=ON \
    -DLLVM_ENABLE_PROJECTS="" \
